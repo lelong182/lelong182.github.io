92ffa55c48eedd2d8b3289337afa010998a4e2d4
LeVuThietLong <levuthietlong@yahoo.com>
2016-03-16 15:38:12 +0700

    update

--- az_site/assets/scripts/functions.js
+++ az_site/assets/scripts/functions.js
@@ -100,7 +100,7 @@
             css3: false,
             scrollingSpeed: 1000,
             fitToSection: false,
-            responsiveWidth: 1400
+            responsiveWidth: 1140
         });
 
 
