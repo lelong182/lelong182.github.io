f9e6fadeeb69276b9de29e6773c96871213ec859
LeVuThietLong <levuthietlong@yahoo.com>
2016-04-27 21:41:22 +0700

    add citibank

---
+++ citibank/assets/scripts/functions.js
@@ -0,0 +1,58 @@
+(function($) {
+
+    "use strict";
+
+    $.fn.adtop_animation = function(effect, callback) {
+        this.addClass(effect + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass(effect + ' animated');
+            if (typeof callback === 'function') {
+                callback.call(this);
+            }
+        });
+    };
+
+    window.loading_screen = window.pleaseWait({
+        logo: "assets/images/logo.png",
+        backgroundColor: '#00A7E6',
+        loadingHtml: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>'
+    });
+
+    $(window).on('load', function() {
+        window.loading_screen.finish();
+        $('body').css('opacity', 1);
+    });
+
+    $(document).ready(function() {
+
+        /* =================================
+        ===  Minimal Menu                 ====
+        =================================== */
+        $('.minimal-menu').before('<label class=\"minimal-menu-button\" for=\"mobile-nav\"><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span></label><input class=\"minimal-menu-button\" type=\"checkbox\" id=\"mobile-nav\" name=\"mobile-nav\" />');
+        $('.minimal-menu').find('ul.sub-menu').parent().addClass('submenu');
+        $('.minimal-menu').find('div.menu-wrapper').parent().addClass('megamenu submenu');
+        $('.minimal-menu').find('ul.sub-menu').before('<input class=\"show-submenu\" type=\"checkbox\" />');
+        $('.minimal-menu').find('div.menu-wrapper').before('<input class=\"show-submenu\" type=\"checkbox\" />');
+
+
+        /* =================================
+        ===  Tooltip                 ====
+        =================================== */
+        if ($('[data-toggle="tooltip"]').length) {
+            $('[data-toggle="tooltip"]').tooltip({
+                container: 'body'
+            });
+        }
+
+
+        /* =================================
+         ===  StickyJS                 ====
+         =================================== */
+        if($('.wrapper').length) {
+            $("header.header").sticky({
+                topSpacing: 0
+            });
+        }
+
+
+    });
+})(window.jQuery);
