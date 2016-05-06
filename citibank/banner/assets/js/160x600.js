d437b92b5c0ad134047781311fe74cc2126bfa80
LeVuThietLong <levuthietlong@yahoo.com>
2016-05-06 17:43:32 +0700

    update

--- citibank/banner/assets/js/160x600.js
+++ citibank/banner/assets/js/160x600.js
@@ -1,221 +1,216 @@
-(function($) {
+$(function() {
 
-    "use strict";
+    var frame_1_img_1 = $('.frame-1 .img-1'),
+        frame_1_img_2 = $('.frame-1 .img-2'),
+        frame_1_text_1 = $('.frame-1 .text-1'),
+        frame_1_text_2 = $('.frame-1 .text-2'),
+        frame_1_text_3 = $('.frame-1 .text-3'),
+        frame_2_text_1 = $('.frame-2 .text-1'),
+        frame_2_text_2 = $('.frame-2 .text-2'),
+        frame_2_img_content_1 = $('.frame-2 .img-content-1'),
+        frame_2_img_content_2 = $('.frame-2 .img-content-2'),
+        frame_2_img_content_3 = $('.frame-2 .img-content-3'),
+        frame_2_img_content_4 = $('.frame-2 .img-content-4'),
+        frame_2_img_content_5 = $('.frame-2 .img-content-5'),
+        frame_3_text_1 = $('.frame-3 .text-1'),
+        frame_3_text_2 = $('.frame-3 .text-2'),
+        frame_3_link = $('.frame-3 .link'),
+        frame_3_right_box = $('.frame-3 .right-box'),
+        frame_3_right_box_span = $('.frame-3 .right-box > span'),
+        replay = $('.replay');
 
-    $(document).ready(function() {
-
-        var frame_1_img_1 = $('.frame-1 .img-1'),
-            frame_1_img_2 = $('.frame-1 .img-2'),
-            frame_1_text_1 = $('.frame-1 .text-1'),
-            frame_1_text_2 = $('.frame-1 .text-2'),
-            frame_1_text_3 = $('.frame-1 .text-3'),
-            frame_2_text_1 = $('.frame-2 .text-1'),
-            frame_2_text_2 = $('.frame-2 .text-2'),
-            frame_2_img_content_1 = $('.frame-2 .img-content-1'),
-            frame_2_img_content_2 = $('.frame-2 .img-content-2'),
-            frame_2_img_content_3 = $('.frame-2 .img-content-3'),
-            frame_2_img_content_4 = $('.frame-2 .img-content-4'),
-            frame_2_img_content_5 = $('.frame-2 .img-content-5'),
-            frame_3_text_1 = $('.frame-3 .text-1'),
-            frame_3_text_2 = $('.frame-3 .text-2'),
-            frame_3_link = $('.frame-3 .link'),
-            frame_3_right_box = $('.frame-3 .right-box'),
-            frame_3_right_box_span = $('.frame-3 .right-box > span'),
-            replay = $('.replay');
-
-        var tl = new TimelineMax({
-            paused: true
-        });
-        tl
-            .fromTo(frame_1_text_1, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '+=0.3')
-            .fromTo(frame_1_text_2, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '-=0.3')
-            .fromTo(frame_1_text_3, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '-=0.3')
-            .fromTo(frame_1_img_1, 0.8, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.3')
-            .fromTo(frame_1_img_2, 0.8, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.8')
-            .to(frame_1_text_1, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            })
-            .to(frame_1_text_2, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            }, '-=0.6')
-            .to(frame_1_text_3, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            }, '-=0.6')
-            .to(frame_1_img_1, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            }, '-=0.2')
-            .to(frame_1_img_2, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            }, '-=0.6')
-            .fromTo(frame_2_text_1, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '-=0.2')
-            .fromTo(frame_2_text_2, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '-=0.4')
-            .fromTo(frame_2_img_content_1, 0.5, {
-                x: 40,
-                autoAlpha: 0
-            }, {
-                x: 0,
-                autoAlpha: 1,
-                ease: Expo.easeInOut
-            }, '-=0.4')
-            .to(frame_2_img_content_1, 0.6, {
-                autoAlpha: 0,
-                ease: Power3.easeOut
-            }, '+=0.7')
-            .fromTo(frame_2_img_content_2, 0.8, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.5')
-            .to(frame_2_img_content_2, 0.6, {
-                autoAlpha: 0,
-                ease: Power3.easeOut
-            }, '+=0.5')
-            .fromTo(frame_2_img_content_3, 0.8, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.5')
-            .to(frame_2_img_content_3, 0.6, {
-                autoAlpha: 0,
-                ease: Power3.easeOut
-            }, '+=0.5')
-            .fromTo(frame_2_img_content_4, 0.8, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.5')
-            .to(frame_2_img_content_4, 0.6, {
-                autoAlpha: 0,
-                ease: Power3.easeOut
-            }, '+=0.5')
-            .fromTo(frame_2_img_content_5, 0.8, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.5')
-            .to(frame_2_text_1, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            })
-            .to(frame_2_text_2, 0.6, {
-                autoAlpha: 0,
-                y: 20,
-                ease: Power4.easeOut
-            }, '-=0.6')
-            .to(frame_2_img_content_5, 0.5, {
-                x: 40,
-                autoAlpha: 0,
-                ease: Expo.easeInOut
-            }, '-=0.5')
-            .fromTo(frame_3_text_1, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '-=0.2')
-            .fromTo(frame_3_text_2, 0.6, {
-                y: 20,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Back.easeOut.config(1)
-            }, '-=0.3')
-            .fromTo(frame_3_link, 0.6, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '-=0.2')
-            .fromTo(frame_3_right_box, 0.5, {
-                y: 40,
-                autoAlpha: 0
-            }, {
-                y: 0,
-                autoAlpha: 1,
-                ease: Power4.easeInOut
-            }, '-=0.5')
-            .fromTo(frame_3_right_box_span, 0.6, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power3.easeOut
-            }, '+=0.1')
-            .fromTo(replay, 0.6, {
-                autoAlpha: 0
-            }, {
-                autoAlpha: 1,
-                ease: Power2.easeOut
-            }, '-=0.2');
-
-        $(this).on('click', '.replay', function() {
-            tl.restart();
-        });
-
-        $(window).on('load', function() {
-            tl.play();
-        });
+    var tl = new TimelineMax({
+        paused: true
     });
-})(window.jQuery);
+    tl
+        .fromTo(frame_1_text_1, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '+=0.3')
+        .fromTo(frame_1_text_2, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '-=0.3')
+        .fromTo(frame_1_text_3, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '-=0.3')
+        .fromTo(frame_1_img_1, 0.8, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.3')
+        .fromTo(frame_1_img_2, 0.8, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.8')
+        .to(frame_1_text_1, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        })
+        .to(frame_1_text_2, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        }, '-=0.6')
+        .to(frame_1_text_3, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        }, '-=0.6')
+        .to(frame_1_img_1, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        }, '-=0.2')
+        .to(frame_1_img_2, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        }, '-=0.6')
+        .fromTo(frame_2_text_1, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '-=0.2')
+        .fromTo(frame_2_text_2, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '-=0.4')
+        .fromTo(frame_2_img_content_1, 0.5, {
+            x: 40,
+            autoAlpha: 0
+        }, {
+            x: 0,
+            autoAlpha: 1,
+            ease: Expo.easeInOut
+        }, '-=0.4')
+        .to(frame_2_img_content_1, 0.6, {
+            autoAlpha: 0,
+            ease: Power3.easeOut
+        }, '+=0.7')
+        .fromTo(frame_2_img_content_2, 0.8, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.5')
+        .to(frame_2_img_content_2, 0.6, {
+            autoAlpha: 0,
+            ease: Power3.easeOut
+        }, '+=0.5')
+        .fromTo(frame_2_img_content_3, 0.8, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.5')
+        .to(frame_2_img_content_3, 0.6, {
+            autoAlpha: 0,
+            ease: Power3.easeOut
+        }, '+=0.5')
+        .fromTo(frame_2_img_content_4, 0.8, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.5')
+        .to(frame_2_img_content_4, 0.6, {
+            autoAlpha: 0,
+            ease: Power3.easeOut
+        }, '+=0.5')
+        .fromTo(frame_2_img_content_5, 0.8, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.5')
+        .to(frame_2_text_1, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        })
+        .to(frame_2_text_2, 0.6, {
+            autoAlpha: 0,
+            y: 20,
+            ease: Power4.easeOut
+        }, '-=0.6')
+        .to(frame_2_img_content_5, 0.5, {
+            x: 40,
+            autoAlpha: 0,
+            ease: Expo.easeInOut
+        }, '-=0.5')
+        .fromTo(frame_3_text_1, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '-=0.2')
+        .fromTo(frame_3_text_2, 0.6, {
+            y: 20,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Back.easeOut.config(1)
+        }, '-=0.3')
+        .fromTo(frame_3_link, 0.6, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '-=0.2')
+        .fromTo(frame_3_right_box, 0.5, {
+            y: 40,
+            autoAlpha: 0
+        }, {
+            y: 0,
+            autoAlpha: 1,
+            ease: Power4.easeInOut
+        }, '-=0.5')
+        .fromTo(frame_3_right_box_span, 0.6, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power3.easeOut
+        }, '+=0.1')
+        .fromTo(replay, 0.6, {
+            autoAlpha: 0
+        }, {
+            autoAlpha: 1,
+            ease: Power2.easeOut
+        }, '-=0.2');
+
+    $(this).on('click', '.replay', function() {
+        tl.restart();
+    });
+
+    $(window).on('load', function() {
+        tl.play();
+    });
+});
--- citibank/banner/gulpfile.js
+++ citibank/banner/gulpfile.js
@@ -7,33 +7,45 @@ var uglify = require("gulp-uglify"),
     concat = require("gulp-concat"),
     concatCss = require("gulp-concat-css"),
     minifyCss = require("gulp-minify-css"),
-    autoprefix = require("gulp-autoprefixer");
+    autoprefix = require("gulp-autoprefixer"),
+    imagemin = require('gulp-imagemin');
 
 // JS concat, strip debugging and minify
 gulp.task('js', function() {
     gulp.src([
-        'assets/js/libs/jquery-1.11.3.min.js',
-        'assets/js/libs/TweenMax.min.js',
-        'assets/js/728x90.js'
+        'assets/js/libs/cash.min.js',
+        'assets/js/libs/plugins/CSSPlugin.min.js',
+        'assets/js/libs/easing/EasePack.min.js',
+        'assets/js/libs/TweenLite.min.js',
+        'assets/js/libs/TimelineMax.min.js',
+        'assets/js/160x600.js'
     ])
         .pipe(concat('scripts.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('728x90/js/'));
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(gulp.dest('160x600/js/'));
 });
 
 // CSS concat, autoprefix, minify
 gulp.task('css', function() {
     gulp.src([
-        'assets/css/animate.min.css',
-        'assets/css/728x90.css'
+        'assets/css/160x600.css'
     ])
         .pipe(concatCss('styles.css'))
         .pipe(autoprefix('last 2 versions'))
         .pipe(minifyCss({
             compatibility: 'ie8'
         }))
-        .pipe(gulp.dest('728x90/css/'));
+        .pipe(gulp.dest('160x600/css/'));
+});
+
+//Images compression
+gulp.task('img', function() {
+    gulp.src('160x600/origin_images/*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('160x600/images'));
 });
 
 // watch for JS/CSS changes
@@ -46,5 +58,6 @@ gulp.task('watch', function() {
 gulp.task('default', [
     'js',
     'css',
+    'img',
     'watch'
 ]);
