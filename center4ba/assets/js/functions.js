0a3055e6b7d8372dd2d570972e26a33c0e543c2a
LeVuThietLong <levuthietlong@yahoo.com>
2016-05-20 07:46:13 +0700

    update

--- center4ba/assets/js/functions.js
+++ center4ba/assets/js/functions.js
@@ -11,6 +11,10 @@
         });
     };
 
+    String.prototype.capitalizeFirstLetter = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
+
     window.loading_screen = window.pleaseWait({
         logo: "assets/images/logo.png",
         backgroundColor: '#ecf0f1',
@@ -53,38 +57,201 @@
         /* =================================
          ===  Custom Carousel                 ====
          =================================== */
-        $('.list-items').slick({
-            infinite: true,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            responsive: [{
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            }, {
-                breakpoint: 858,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }, {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    fade: true
-                }
-            }]
-        });
-        $(this).on('mouseover', '.list-items .item', function() {
-            $(this).addClass('open');
-        });
-        $(this).on('mouseout', '.list-items .item', function() {
-            $('.list-items .item').removeClass('open');
-        });
+        if ($('.list-items').length) {
+            $('.list-items').slick({
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [{
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                }, {
+                    breakpoint: 858,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        fade: true
+                    }
+                }]
+            });
+            $(this).on('mouseover', '.list-items .item', function() {
+                $(this).addClass('open');
+            });
+            $(this).on('mouseout', '.list-items .item', function() {
+                $('.list-items .item').removeClass('open');
+            });
+        }
+
+
+        /* =================================
+         ===  Dashboard                 ====
+         =================================== */
+        if ($('.dashboard').length) {
+            myChart('chart-1', {
+                chartData: [{
+                    'year': '2012',
+                    'Income': 24.8,
+                    'Expenses': 17.5
+                }, {
+                    'year': '2013',
+                    'Income': 30.1,
+                    'Expenses': 23.9
+                }, {
+                    'year': '2014',
+                    'Income': 29.5,
+                    'Expenses': 25.1
+                }, {
+                    'year': '2015',
+                    'Income': 24.6,
+                    'Expenses': 25
+                }, {
+                    'year': '2016',
+                    'Income': 21.3,
+                    'Expenses': 20
+                }],
+                arrFields: [{
+                    name: 'Income',
+                    color: '#95a5a6'
+                }, {
+                    name: 'Expenses',
+                    color: '#34495e'
+                }]
+            });
+        }
 
 
     });
 })(window.jQuery);
+
+function myChart(chartId, opts) {
+    if (opts == null) {
+        opts = {};
+    }
+    var settings = $.extend({
+        chartData: [],
+        arrFields: []
+    }, opts);
+
+    var configChart = function() {
+        var chart = new AmCharts.AmSerialChart();
+        chart.startDuration = 1;
+        chart.autoMarginOffset = 0;
+        chart.marginTop = 10;
+        chart.marginRight = 10;
+        chart.marginBottom = 0;
+        chart.marginLeft = 0;
+        chart.dataProvider = settings.chartData;
+        chart.categoryField = "year";
+        chart.rotate = true;
+        chart.language = 'vi';
+        chart.numberFormatter = {
+            precision: -1,
+            decimalSeparator: ",",
+            thousandsSeparator: "."
+        };
+        chart.balloon.borderThickness = 1;
+        chart.balloon.shadowAlpha = 0;
+        chart.balloon.fillAlpha = 1;
+        chart.balloon.fontSize = 13;
+        chart.responsive = {
+            "enabled": true
+        }
+
+        var categoryAxis = chart.categoryAxis;
+        categoryAxis.gridPosition = 'start';
+        categoryAxis.position = 'left';
+        categoryAxis.gridPosition = true;
+        categoryAxis.minorGridEnabled = true;
+        categoryAxis.axisColor = "#cccccc";
+        categoryAxis.dateFormats = [{
+            period: 'fff',
+            format: 'JJ:NN:SS'
+        }, {
+            period: 'ss',
+            format: 'JJ:NN:SS'
+        }, {
+            period: 'mm',
+            format: 'JJ:NN'
+        }, {
+            period: 'hh',
+            format: 'JJ:NN'
+        }, {
+            period: 'DD',
+            format: 'DD-MM'
+        }, {
+            period: 'WW',
+            format: 'DD'
+        }, {
+            period: 'MM',
+            format: 'MMM'
+        }, {
+            period: 'YYYY',
+            format: 'YYYY'
+        }];
+
+        var valueAxis = new AmCharts.ValueAxis();
+        valueAxis.axisColor = "#cccccc";
+        valueAxis.position = "top";
+        valueAxis.axisAlpha = 0;
+        chart.addValueAxis(valueAxis);
+
+        for (var i = 0; i < settings.arrFields.length; i++) {
+            var graph = new AmCharts.AmGraph();
+            graph.fillAlphas = 0.8;
+            graph.lineAlpha = 0.2;
+            graph.type = 'column';
+            graph.negativeLineColor = "#e74c3c";
+            var balloonTextTmp = '[[value]]';
+            $.each(settings.arrFields[i], function(key, value) {
+                if (key == 'name') {
+                    graph.valueField = value;
+                    balloonTextTmp = value.capitalizeFirstLetter() + ": " + balloonTextTmp;
+                }
+                if (key == 'color') {
+                    graph.lineColor = value;
+                }
+                if (key == 'unit') {
+                    balloonTextTmp = balloonTextTmp + value;
+                }
+                graph.balloonText = balloonTextTmp;
+            });
+            chart.addGraph(graph);
+        }
+
+        var chartCursor = new AmCharts.ChartCursor();
+        chartCursor.cursorPosition = "mouse";
+        chartCursor.pan = true;
+        chartCursor.categoryBalloonColor = '#3498db';
+        chartCursor.cursorColor = '#3498db';
+        chartCursor.valueLineEnabled = true;
+        chartCursor.valueLineBalloonEnabled = true;
+        chart.addChartCursor(chartCursor);
+
+        var chartScrollbar = new AmCharts.ChartScrollbar();
+        chartScrollbar.dragIconWidth = 18;
+        chartScrollbar.scrollbarHeight = 5;
+        chartScrollbar.backgroundAlpha = 0.1;
+        chartScrollbar.backgroundColor = "#868686";
+        chartScrollbar.selectedBackgroundColor = "#3498db";
+        chartScrollbar.selectedBackgroundAlpha = 1;
+        chartScrollbar.offset = 20;
+        chart.addChartScrollbar(chartScrollbar);
+
+        chart.write(chartId);
+    };
+
+    if (AmCharts.isReady) {
+        configChart();
+    } else {
+        AmCharts.ready(configChart);
+    }
+}
