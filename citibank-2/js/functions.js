378de41564dd1f0819b645b55cd48ebfad8dfa02
LeVuThietLong <levuthietlong@yahoo.com>
2016-06-28 21:28:37 +0700

    update

--- citibank-2/js/functions.js
+++ citibank-2/js/functions.js
@@ -25,11 +25,8 @@
 
 
     window.loading_screen = window.pleaseWait({
-
         logo: "images/logo.png",
-
         backgroundColor: '#00A7E6',
-
         loadingHtml: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>'
 
     });
@@ -48,18 +45,46 @@
 
     $(document).ready(function() {
 
+        /* =================================
+         ===  Animation                 ====
+         =================================== */
+        $('.allinone_bannerRotator').addClass('hidden');
+        $('.first-slide')
+            .delay(2000)
+            .velocity({
+                translateX: 0,
+                opacity: 1
+            }, {
+                duration: 1200
+            })
+            .delay(1500)
+            .velocity({
+                opacity: 0
+            }, {
+                duration: 1000,
+                complete: function(elements) {
+                    $('.first-slide').remove()
+                    $('.slideshow-container').css({
+                        opacity: 1
+                    });
+                }
+            });
+        $('.bottom-slideshow .wrap-img')
+            .delay(2500)
+            .velocity({
+                translateX: 0,
+                opacity: 1
+            }, {
+                duration: 800
+            });
 
 
         /* =================================
-
-        ===  Tooltip                 ====
-
-        =================================== */
-
+         ===  Tooltip                 ====
+         =================================== */
         if ($('[data-toggle="tooltip"]').length) {
 
             $('[data-toggle="tooltip"]').tooltip({
-
                 container: 'body'
 
             });
@@ -71,11 +96,8 @@
 
 
         /* =================================
-
-        ===  Disable Tab                 ====
-
-        =================================== */
-
+         ===  Disable Tab                 ====
+         =================================== */
         $(".nav-tabs li.disabled a[data-toggle=tab]").on("click", function(e) {
 
             e.preventDefault();
@@ -89,35 +111,21 @@
 
 
         /* =================================
-
          ===  Slideshow                 ====
-
          =================================== */
-
         if ($('.slideshow').length) {
 
             $('.slideshow').allinone_bannerRotator({
-
                 defaultEffect: 'fade',
-
                 autoPlay: 5,
-
                 skin: 'universal',
-
                 width: 1366,
-
                 height: 651,
-
                 responsive: true,
-
                 responsiveRelativeToBrowser: false,
-
                 showCircleTimer: false,
-
                 showPreviewThumbs: false,
-
                 showAllControllers: false,
-
                 showBottomNav: false
 
             });
@@ -129,55 +137,34 @@
 
 
         /* =================================
-
          ===  Custom Carousel                 ====
-
          =================================== */
-
         $('.bottom-slideshow .list-imgs').slick({
-
             arrows: false,
-
             infinite: true,
-
             fade: true,
-
             autoplaySpeed: 4000,
-
             autoplay: true,
-
             speed: 800
 
         });
 
         $('.hotel-content .list-contents').slick({
-
             arrows: false,
-
             infinite: true,
-
             fade: true,
-
             autoplaySpeed: 5000,
-
             autoplay: true,
-
             speed: 1000
 
         });
 
         $('.hotel-image .list-imgs').slick({
-
             arrows: false,
-
             infinite: true,
-
             fade: true,
-
             autoplaySpeed: 4000,
-
             autoplay: true,
-
             speed: 800
 
         });
@@ -217,11 +204,8 @@
 
 
         /* =================================
-
          ===  Apply Bar                 ====
-
          =================================== */
-
         $(window).scroll(function() {
 
             if (!$('.apply').hasClass('closed')) {
@@ -247,15 +231,13 @@
             $('.apply').fadeOut();
 
             $('.footer').css({
-
                 'margin-bottom': 0
 
             });
 
         });
 
-
-        $('a[id^="link-"]').on('click', function(){
+        $('a[id^="link-"]').on('click', function() {
             $('div[id^="Modal-"]').modal('hide');
         });
 
