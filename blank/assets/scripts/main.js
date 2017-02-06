940e72e2b7ca980085d9e6601cd7c344e9e76527
LeVuThietLong <levuthietlong@yahoo.com>
2017-02-06 11:32:16 +0700

    add blank project

---
+++ blank/assets/scripts/main.js
@@ -0,0 +1,38 @@
+(function ($) {
+
+    "use strict";
+
+    $.fn.customAnimate = function (effect, callback) {
+        this.addClass(effect + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass(effect + ' animated');
+            if (typeof callback === 'function') {
+                callback.call(this);
+            }
+        });
+    };
+
+    window.loading_screen = window.pleaseWait({
+        logo: "dist/images/logo.png",
+        backgroundColor: '#fff',
+        loadingHtml: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>'
+    });
+
+    $(window).on('load', function () {
+        window.loading_screen.finish();
+        $('body').css('opacity', 1);
+    });
+
+    $(document).ready(function () {
+
+        /* =================================
+         ===  Test function                 ====
+         =================================== */
+        test();
+
+    });
+
+})(window.jQuery);
+
+function test() {
+    console.log('test');
+}
