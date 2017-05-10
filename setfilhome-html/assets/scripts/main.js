5699215700ad8d94dbff56237ead6624a1d892ea
LeVuThietLong <levuthietlong@yahoo.com>
2017-05-10 12:05:17 +0700

    update 2

--- setfilhome-html/assets/scripts/main.js
+++ setfilhome-html/assets/scripts/main.js
@@ -95,11 +95,9 @@
     function projectGallery2() {
         projectGallery2 = $("#gallery-2").unitegallery({
             gallery_theme: "tiles",
-            tiles_type: "justified",            //must option for the tiles - justified type
-            tiles_justified_row_height: 300,    //base row height of the justified type
-            tiles_justified_space_between: 3,   //space between the tiles justified type
-            tiles_set_initial_height: true,     //columns type related only
-            tiles_enable_transition: true,      //enable transition when screen width change
+            tiles_type: "justified",      
+            gallery_min_width: 250,  
+            tiles_justified_row_height: 300
         });
     }
 
