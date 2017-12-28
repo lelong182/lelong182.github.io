ced8c68ea9798d74572d79c7e2d9d195a41bdb14
lelong182 <levuthietlong@gmail.com>
2017-12-28 16:37:44 +0700

    delete ved

--- ved/airpay/assets/index.js
+++
@@ -1,5 +0,0 @@
-import './scss/styles.scss';
-import './masterslider/masterslider.min';
-import '../libs/slick-carousel/slick/slick.min';
-import '../node_modules/jarallax/dist/jarallax.min';
-import './scripts/main';
