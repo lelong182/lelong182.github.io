2831adfd01ca2b6ce8998cb6c8b7e422b0916920
lelong182 <levuthietlong@yahoo.com>
2018-08-06 16:07:28 +0700

    add slider

---
+++ slider-plugin/gulpfile.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  uglify = require('gulp-uglify'),
+  stripDebug = require('gulp-strip-debug'),
+  autoprefixer = require('gulp-autoprefixer'),
+  concatCss = require('gulp-concat-css'),
+  concat = require('gulp-concat'),
+  cleanCss = require('gulp-clean-css')
+
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/styles'))
+})
+
+gulp.task('css', ['sass'], function () {
+  gulp.src([
+    'assets/styles/slick.css',
+    'assets/styles/styles.css'
+  ])
+    .pipe(concatCss('main.css'))
+    .pipe(autoprefixer())
+    .pipe(cleanCss({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest('dist/styles'))
+})
+
+gulp.task('js', function () {
+  gulp.src([
+    'assets/scripts/libs/jquery.min.js',
+    'assets/scripts/libs/slick.js',
+    'assets/scripts/main.js'
+  ])
+    .pipe(concat('main.js'))
+    // .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+})
+
+gulp.task('copy-fonts', function () {
+  return gulp.src(['assets/fonts/**/*'])
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('copy-imgs', function () {
+  return gulp.src(['assets/images/**/*'])
+    .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('watch', function () {
+  gulp.watch('assets/scripts/**/*.js', ['js'])
+  gulp.watch('assets/scss/**/*.scss', ['css'])
+})
+
+gulp.task('dev', [
+  'copy-fonts',
+  'copy-imgs',
+  'css',
+  'js',
+  'watch'
+])
+
+gulp.task('default', [
+  'copy-fonts',
+  'copy-imgs',
+  'css',
+  'js'
+])
