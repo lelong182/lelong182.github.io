4957b5a9d8a4cd2e0f7e47194a8218e00bdeb1e6
lelong182 <levuthietlong@yahoo.com>
2018-08-06 18:01:51 +0700

    add slider 2

--- slider-plugin/assets/scripts/main.js
+++ slider-plugin/assets/scripts/main.js
@@ -4,12 +4,33 @@
 
   $(document).ready(function () {
 
-    $('#slider-1').slick({
+    $('.wrap-slider .style-1').slick({
       variableWidth: true,
       arrows: false,
       dots: true,
       centerMode: true,
       slidesToShow: 3
+    })
+
+    $('.wrap-slider .style-2').slick({
+      variableWidth: true,
+      arrows: false,
+      slidesToShow: 3
+    })
+
+    $('.wrap-slider .style-2').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      if (nextSlide === 0) {
+        var $nextClone = $('.slick-current').next('.slick-cloned')
+        setTimeout(function () {
+          $nextClone.addClass('slick-current')
+        })
+      }
+      if (nextSlide === 4) {
+        var $prevClone = $('.slick-current').prev('.slick-cloned')
+        setTimeout(function () {
+          $prevClone.addClass('slick-current')
+        })
+      }
     })
 
   })
