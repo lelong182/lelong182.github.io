b61177325a4a86f2c488a45f045ae89f56953074
lelong182 <levuthietlong@yahoo.com>
2018-08-07 09:37:13 +0700

    update

--- slider-plugin/assets/scripts/main.js
+++ slider-plugin/assets/scripts/main.js
@@ -4,7 +4,7 @@
 
   $(document).ready(function () {
 
-    $(window).on('load', function () {
+    setTimeout(function () {
       $('.wrap-slider .style-1').slick({
         variableWidth: true,
         arrows: false,
@@ -18,21 +18,25 @@
         arrows: false,
         slidesToShow: 3
       })
+    }, 100)
 
-      $('.wrap-slider .style-2').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-        if (nextSlide === 0) {
-          var $nextClone = $('.slick-current').next('.slick-cloned')
-          setTimeout(function () {
-            $nextClone.addClass('slick-current')
-          })
-        }
-        if (nextSlide === 4) {
-          var $prevClone = $('.slick-current').prev('.slick-cloned')
-          setTimeout(function () {
-            $prevClone.addClass('slick-current')
-          })
-        }
-      })
+    setTimeout(function () {
+      $('.wrap-slider').removeClass('hidden')
+    }, 150)
+
+    $('.wrap-slider .style-2').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      if (nextSlide === 0) {
+        var $nextClone = $('.slick-current').next('.slick-cloned')
+        setTimeout(function () {
+          $nextClone.addClass('slick-current')
+        })
+      }
+      if (nextSlide === 4) {
+        var $prevClone = $('.slick-current').prev('.slick-cloned')
+        setTimeout(function () {
+          $prevClone.addClass('slick-current')
+        })
+      }
     })
 
   })
