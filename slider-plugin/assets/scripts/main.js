f32ed7b8d3e4a1c22b45985229749b387b52a362
lelong182 <levuthietlong@yahoo.com>
2018-08-07 14:20:54 +0700

    add alert

--- slider-plugin/assets/scripts/main.js
+++ slider-plugin/assets/scripts/main.js
@@ -5,40 +5,45 @@
   var countItem2 = 0
 
   window.onload = function () {
-    $('.wrap-slider .style-1').slick({
-      variableWidth: true,
-      arrows: false,
-      dots: true,
-      centerMode: true,
-      slidesToShow: 3
-    })
 
-    $('.wrap-slider .style-2').slick({
-      variableWidth: true,
-      arrows: false,
-      slidesToShow: 3
-    })
+    setTimeout(function () {
+      $('.wrap-slider .style-1').slick({
+        variableWidth: true,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        slidesToShow: 3
+      })
 
-    countItem2 = $('.wrap-slider .style-2 .slick-slide:not(.slick-cloned)').length
+      $('.wrap-slider .style-2').slick({
+        variableWidth: true,
+        arrows: false,
+        slidesToShow: 3
+      })
+
+      countItem2 = $('.wrap-slider .style-2 .slick-slide:not(.slick-cloned)').length
+
+      $('.wrap-slider .style-2').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        if (nextSlide === 0) {
+          var $nextClone = $('.slick-current').next('.slick-cloned')
+          setTimeout(function () {
+            $nextClone.addClass('slick-current')
+          })
+        }
+        if (nextSlide === countItem2 - 1) {
+          var $prevClone = $('.slick-current').prev('.slick-cloned')
+          setTimeout(function () {
+            $prevClone.addClass('slick-current')
+          })
+        }
+      })
+
+      alert(111)
+    }, 100)
 
     setTimeout(function () {
       $('.wrap-slider').removeClass('hidden')
     }, 150)
-
-    $('.wrap-slider .style-2').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-      if (nextSlide === 0) {
-        var $nextClone = $('.slick-current').next('.slick-cloned')
-        setTimeout(function () {
-          $nextClone.addClass('slick-current')
-        })
-      }
-      if (nextSlide === countItem2 - 1) {
-        var $prevClone = $('.slick-current').prev('.slick-cloned')
-        setTimeout(function () {
-          $prevClone.addClass('slick-current')
-        })
-      }
-    })
   }
 
 })(window.jQuery)
